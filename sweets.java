248d5edb10396f66de9c6d9736062d610746702a
shadatsk40 <38094421+shadatsk40@users.noreply.github.com>
2020-07-20 15:10:00 +0530

    Create sweets.java

---
+++ sweets.java
@@ -0,0 +1,23 @@
+package com.sweets;
+import java.util.*;
+
+public class sweets {
+	Scanner input=new Scanner(System.in);
+	public static int COUNTXz=0;
+	public void SWEETS1()
+	{
+		System.out.println("enter no.of sweets");
+		int numbersx1=input.nextInt();
+		System.out.println("sweet 1:"+numbersx1);
+		COUNTXz=COUNTXz+numbersx1;
+	}
+	public void SWEETS2()
+	{
+		System.out.println("enter no.of sweets");
+		int numbersx2=input.nextInt();
+		System.out.println("sweet 2:"+numbersx2);
+		COUNTXz=COUNTXz+numbersx2;
+	}
+	
+
+}
