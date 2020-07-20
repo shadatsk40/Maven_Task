c45f0fd0dec2ae4edead73a6d41fe9a4f45a9274
shadatsk40 <38094421+shadatsk40@users.noreply.github.com>
2020-07-20 15:05:57 +0530

    Create gifts.java

---
+++ gifts.java
@@ -0,0 +1,18 @@
+package com.gift;
+
+import com.chocolates.chocolates;
+import com.sweets.sweets;
+
+public class gifts extends chocolates{
+	
+	public static void main(String args[]) {
+	sweets Sweet=new sweets();
+	chocolates Chocos=new chocolates();
+	Sweet.SWEETS1();
+	Sweet.SWEETS1();
+	Chocos.CHOCOLATE1();
+	Chocos.CHOCOLATE2();
+	
+
+}
+}
